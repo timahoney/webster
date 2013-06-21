13b5a67cd4a20666f28bcbea5dec5311ae4aec13
Tim Mahoney <tim.mahoney@me.com>
2013-06-21 14:29:29 -0700

    Added a back button to the info page.

--- site/scripts/info-page.js
+++ site/scripts/info-page.js
@@ -3,3 +3,17 @@ var InfoPage = function() {
 };
 
 InfoPage.prototype = new Page();
+
+InfoPage.prototype._didLoad = function() {
+  var backButton = this.element.querySelector('.back-button');
+  backButton.onclick = this._onClickBack.bind(this);
+};
+
+InfoPage.prototype._onClickBack = function(event) {
+  if (event.metaKey || event.ctrlKey) {
+    return;
+  }
+
+  event.preventDefault();
+  window.history.back();
+};
