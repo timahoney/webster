dec7b99d8c6eb3937e616e41a7af6fbbdb7ed273
Tim Mahoney <tim.mahoney@me.com>
2013-06-21 10:32:31 -0700

    Added analytics.

--- site/scripts/analytics.js
+++ site/scripts/analytics.js
@@ -3,7 +3,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-41303216-1', 'easydocs.org');
+ga('create', 'UA-41925840-1', 'webster.io');
 ga('send', 'pageview');
 
 window.addEventListener('message', function(event) {
